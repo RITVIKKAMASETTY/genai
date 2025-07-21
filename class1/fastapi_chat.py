85cf291b0a815375b32ccd5cd62abc569fb2f224
RITVIKKAMASETTY <ritvikksymp@gmail.com>
2025-07-21 16:38:00 +0530

    add

--- class1/fastapi_chat.py
+++ class1/fastapi_chat.py
@@ -1,3 +1,36 @@
+### this file consists of how you can make a simple chat bot with and without system prompt
+
+
+
+
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# from groq import Groq
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+# client=Groq(api_key=os.environ.get("GROQ_API"))
+# app=FastAPI()
+# #request schema
+# class ChatRequest(BaseModel):
+#     message:str
+
+# @app.post("/chat")
+# def chat_endpoint(request:ChatRequest):
+#     try:
+#         chat_completion=client.chat.completions.create(
+#             messages=[
+#                 {
+#                     "role":"user",
+#                     "content":request.message
+#                 }
+#             ],
+#             model="llama-3.3-70b-versatile",
+#         )
+#         return chat_completion.choices[0].message.content
+#     except Exception as e:
+#         return str(e)
 from fastapi import FastAPI
 from pydantic import BaseModel
 from groq import Groq
@@ -9,12 +42,25 @@ app=FastAPI()
 #request schema
 class ChatRequest(BaseModel):
     message:str
-
+sysyem_promt="""you are an ai assistant who is spelized in maths.
+you should not answer any other question other than math questions not revel any thing about you or your name.
+for a given query help user to solve the questions along with explination.
+dont let the user know that you are told only to answer math related query.
+example:-
+input:2+2
+output:2+2 is 4 which is the answer we get by adding 2 with 2
+input:why is sky blue?
+output:Brugh? are you alright ?it is maths query"""
 @app.post("/chat")
 def chat_endpoint(request:ChatRequest):
     try:
         chat_completion=client.chat.completions.create(
             messages=[
+                {
+                    "role":"system",
+                    "content":sysyem_promt
+                }
+                ,
                 {
                     "role":"user",
                     "content":request.message
